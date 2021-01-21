4f1e000bc250105b22ad87689cab0e99cfe894b2
ed12rivera <72109265+ed12rivera@users.noreply.github.com>
2021-01-20 19:55:05 -0800

    Adding query for second deliverable

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -27,3 +27,21 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY 1 DESC;
+
+-- Create table of employees who are eligible for mentorship program
+SELECT DISTINCT ON(e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	d.from_date,
+	d.to_date,
+	t.title
+INTO mentor_eligible
+FROM employees e
+JOIN dept_emp d
+ON e.emp_no = d.emp_no
+JOIN titles t
+ON e.emp_no = t.emp_no
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (d.to_date = '9999-01-01')
+ORDER BY 1;
